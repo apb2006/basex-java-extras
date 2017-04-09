13cb30e42cab07369bf9941feb9cfd2962678671
andy <bunce.andy@gmail.com>
2017-04-09 22:56:08 +0100

    format

--- org/apb/modules/TestModule.java
+++ org/apb/modules/TestModule.java
@@ -41,106 +41,109 @@ import net.coobird.thumbnailator.Thumbnailator;
 
 public class TestModule extends QueryModule {
 
-	public static String hello(final String world) {
-		return "Hello.. " + world;
-	}
+    public static String hello(final String world) {
+        return "Hello.. " + world;
+    }
 
-	public static Value sequence() {
-		FElem elem1 = new FElem("root1");
-		FElem elem2 = new FElem("root2");
-		ValueBuilder vb = new ValueBuilder();
-		vb.add(elem1);
-		vb.add(elem2);
-		return vb.value();
-	}
+    public static Value sequence() {
+        FElem elem1 = new FElem("root1");
+        FElem elem2 = new FElem("root2");
+        ValueBuilder vb = new ValueBuilder();
+        vb.add(elem1);
+        vb.add(elem2);
+        return vb.value();
+    }
 
-	public static Value filewalk(final String path) throws IOException {
-		Path startingDir = Paths.get(path);
-		FileWalk pf = new FileWalk();
-		Files.walkFileTree(startingDir, pf);
-		return pf.result();
-	}
+    public static Value filewalk(final String path) throws IOException {
+        Path startingDir = Paths.get(path);
+        FileWalk pf = new FileWalk();
+        Files.walkFileTree(startingDir, pf);
+        return pf.result();
+    }
 
-	@Requires(Permission.NONE)
-	public static ANode create() {
-		FDoc doc = new FDoc("http://www.example.com");
-		FElem elem = new FElem("root").add("attr", "value");
-		doc.add(elem);
-		return doc;
-	}
+    @Requires(Permission.NONE)
+    public static ANode create() {
+        FDoc doc = new FDoc("http://www.example.com");
+        FElem elem = new FElem("root").add("attr", "value");
+        doc.add(elem);
+        return doc;
+    }
 
-	/*
-	 * handle Map as argument if map has key depth return value of key else
-	 * return size of map.
-	 */
-	public static int mapinfo(Map m) throws QueryException {
-		Item key = Str.get("depth");
-		InputInfo ii = new InputInfo("what?", 0, 0);
-        Value v=m.get(key, ii);
-        if(v.isEmpty()){
-        	return m.mapSize();
+    /*
+     * test handling Map as argument 
+     * if map has key depth then
+     *       return value of key 
+     * else
+     * return size of map.
+     */
+    public static int mapinfo(Map m) throws QueryException {
+        Item key = Str.get("depth");
+        InputInfo ii = new InputInfo("what?", 0, 0);
+        Value v = m.get(key, ii);
+        if (v.isEmpty()) {
+            return m.mapSize();
         }
-		Item item = v.itemAt(0);
-		if (item instanceof ANum) {
-			return (int) ((ANum) item).itr(null);
-		} else {
-			return -1;
-		}
+        Item item = v.itemAt(0);
+        if (item instanceof ANum) {
+            return (int) ((ANum) item).itr(null);
+        } else {
+            return -1;
+        }
 
-	}
-	/* stop current execution after timeout ms */
+    }
+    /* stop current execution after timeout ms */
 
-	/* stream test: make a copy */
-	@Requires(Permission.NONE)
-	public static B64Stream thumb(final B64Stream inputStream, final Int num) throws IOException, QueryException {
-		BufferInput b = inputStream.input(new InputInfo(new InputParser("hi")));
-		int width = (int) num.itr();
-		int height = width;
-		ByteArrayOutputStream os = new ByteArrayOutputStream();
-		B64Stream os2 = new B64Stream(new IOContent(os.toByteArray()), IOERR_X);
-		Thumbnailator.createThumbnail(b, os, width, height);
-		return os2;
+    /* stream test: make a copy */
+    @Requires(Permission.NONE)
+    public static B64Stream thumb(final B64Stream inputStream, final Int num) throws IOException, QueryException {
+        BufferInput b = inputStream.input(new InputInfo(new InputParser("hi")));
+        int width = (int) num.itr();
+        int height = width;
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        B64Stream os2 = new B64Stream(new IOContent(os.toByteArray()), IOERR_X);
+        Thumbnailator.createThumbnail(b, os, width, height);
+        return os2;
 
-	}
+    }
 
-	public static InputStream fileInputStream(String file) throws IOException {
-		return (InputStream) new FileInputStream(file);
-	}
+    public static InputStream fileInputStream(String file) throws IOException {
+        return (InputStream) new FileInputStream(file);
+    }
 
-	// @return image format
-	// http://stackoverflow.com/questions/11425521/how-to-get-the-formatexjpen-png-gif-of-image-file-bufferedimage-in-java/31513596#31513596
-	public static String imageFormatName(InputStream input) throws IOException {
-		ImageInputStream stream = ImageIO.createImageInputStream(input);
+    // @return image format
+    // http://stackoverflow.com/questions/11425521/how-to-get-the-formatexjpen-png-gif-of-image-file-bufferedimage-in-java/31513596#31513596
+    public static String imageFormatName(InputStream input) throws IOException {
+        ImageInputStream stream = ImageIO.createImageInputStream(input);
 
-		Iterator<ImageReader> iter = ImageIO.getImageReaders(stream);
-		if (!iter.hasNext()) {
-			return null;
-		}
-		ImageReader reader = (ImageReader) iter.next();
-		ImageReadParam param = reader.getDefaultReadParam();
-		reader.setInput(stream, true, true);
-		BufferedImage bi;
-		try {
-			bi = reader.read(0, param);
-			return reader.getFormatName();
-		} finally {
-			reader.dispose();
-			stream.close();
-		}
-	}
+        Iterator<ImageReader> iter = ImageIO.getImageReaders(stream);
+        if (!iter.hasNext()) {
+            return null;
+        }
+        ImageReader reader = (ImageReader) iter.next();
+        ImageReadParam param = reader.getDefaultReadParam();
+        reader.setInput(stream, true, true);
+        BufferedImage bi;
+        try {
+            bi = reader.read(0, param);
+            return reader.getFormatName();
+        } finally {
+            reader.dispose();
+            stream.close();
+        }
+    }
 
-	/*
-	 * arraylist from iterable
-	 * http://stackoverflow.com/questions/6416706/easy-way
-	 * -to-change-iterable-into-collection/6416921#6416921
-	 */
-	@Requires(Permission.NONE)
-	public static <E> Collection<E> makeCollection(Iterable<E> iter) {
-		Collection<E> list = new ArrayList<E>();
-		for (E item : iter) {
-			list.add(item);
-		}
-		return list;
-	}
+    /*
+     * arraylist from iterable
+     * http://stackoverflow.com/questions/6416706/easy-way
+     * -to-change-iterable-into-collection/6416921#6416921
+     */
+    @Requires(Permission.NONE)
+    public static <E> Collection<E> makeCollection(Iterable<E> iter) {
+        Collection<E> list = new ArrayList<E>();
+        for (E item : iter) {
+            list.add(item);
+        }
+        return list;
+    }
 
 }
